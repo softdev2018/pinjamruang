1db04008ffef5e5249b48832f4ca6ab938f29c09
Dwiko Satriyo <dwikosatriyo@student.uns.ac.id>
2018-03-05 02:09:11 +0700

    penggantian nama button

--- frontend/views/peminjaman/_form.php
+++ frontend/views/peminjaman/_form.php
@@ -74,7 +74,7 @@ $this->registerJs($this->render('_script.js'));
 
                 <br><br>
                 <div class="form-group">
-                    <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+                    <?= Html::submitButton($model->isNewRecord ? 'Ajukan' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
                 </div>
 
                 <?php ActiveForm::end(); ?>
